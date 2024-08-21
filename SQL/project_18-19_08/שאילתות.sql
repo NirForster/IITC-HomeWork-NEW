728e4c2d9049bea5ba2aac2f917462864ab16fb2
NirForster <162992599+NirForster@users.noreply.github.com>
2024-08-21 15:10:25 +0300

    עדכון של חלק מהשאילתות

--- SQL/project_18-19_08/שאילתות.sql
+++ SQL/project_18-19_08/שאילתות.sql
@@ -1,51 +1,62 @@
 --Q1
 --select * from items
 
-----Q2
+
+--Q2
 --select 
 --E.Date,
---i.TotAmount,
---i.UnitDesc,
+--I.TotAmount,
+--I.UnitDesc,
 --quant, 
 --status,
 --e.OrderNo
 --from ExOrders E
---join Invoices i 
---on E.OrderNo = i.OrderNo
+--join Invoices I 
+--on E.OrderNo = I.OrderNo
+--where [Status] = 'closed' and
+--E.Date > '2024-08-01' and E.Date < '2024-08-30' 
+--and e.OrderNo = 2003
 --order by E.Date
 
 
---SELECT 
---    eo.[Date] AS OrderDate,
---    SUM(i.UnitPrice * eo.Quant) AS TotalAmount,
---    GROUP_CONCAT(CONCAT(i.Description, ' (', eo.Quant, ')')) AS ItemsOrdered,
---    eo.[Status] AS OrderStatus
---FROM 
---    ExOrders eo
---JOIN 
---    Items i ON eo.Code = i.Code
---WHERE 
---    (eo.[Status] = 'YOUR_STATUS_CONDITION' OR 'YOUR_STATUS_CONDITION' IS NULL)
---    AND (eo.[Date] BETWEEN 'START_DATE' AND 'END_DATE' OR ('START_DATE' IS NULL AND 'END_DATE' IS NULL))
---    AND (eo.OrderNo = YOUR_ORDER_NUMBER OR YOUR_ORDER_NUMBER IS NULL)
---GROUP BY 
---    eo.[Date], eo.[Status]
---ORDER BY 
---    eo.[Date];
-
-
 --Q3
-select 
-C.CustID, 
-CustName, 
-CustStatus,
-OrderNo,
-Status,
-i.Description,
-Quant,
-i.Freq,
-from Customers C 
-join ExOrders E 
-on C.CustID = E.CustID 
-join items I 
-on I.Code = E.Code
+--select 
+--C.CustID, 
+--C.CustName, 
+--C.CustStatus,
+--EO.OrderNo,
+--EO.Status AS OrderStatus,
+--EO.Quant,
+--I.Description,
+--I.Freq
+--from Customers C 
+--join ExOrders EO
+--on C.CustID = EO.CustID 
+--join items I 
+--on I.Code = EO.Code
+--where C.CustID = 3
+
+
+----Q4
+--select 
+--A.InvNo,
+--A.ReceiptNo,
+--A.Date,
+--A.Amount as TotalToPay,
+--A.CrdtDebt as Paid
+--from Accounting A 
+----join Invoices I
+----on A.InvNo = I.InvNo
+
+
+--Q5
+--select 
+--EO.Date,
+--EO.SuppDate,
+--I.TotAmount,
+--EO.Status as OrderStatus
+--from ExOrders EO
+--join Invoices I 
+--on EO.OrderNo = I.OrderNo
+--where EO.Status = 'Not Shipped' or EO.Status = 'reserved in Progress' 
+--order by SuppDate
